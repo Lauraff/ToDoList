b14e18308df9674a7d9bbfd46cbd1e28ca0388e8
Laura Flaquer <laura.flaquer@outlook.fr>
2019-11-25 13:57:23 +0100

    Connection to database + retrieve data

---
+++ index.js
@@ -0,0 +1,28 @@
+// Import a module
+const http = require('http')
+const url = require('url')
+const qs = require('querystring')
+
+const serverHandle = function (req, res) {
+  const route = url.parse(req.url)
+  const path = route.pathname 
+  const params = qs.parse(route.query)
+
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  if(path==='/'){
+    res.write('TODOLIST \n' )
+  }
+  else if (path === '/hello' && 'name' in params ) {
+    
+  } 
+  else{
+    res.write('error 404')
+  }
+
+  res.end();
+}
+// Declare an http server
+http.createServer(serverHandle).listen(8080)
+
+// curl localhost:8080 or go to http://localhost:8080
